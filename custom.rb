9e5bb61aa1949ae68aa85712ff31e525806af45c
Nathan C <sihoonathan@gmail.com>
2020-03-01 15:34:58 -0800

    my_each done

--- custom.rb
+++ custom.rb
@@ -1,1 +1,13 @@
-test
+module Enumerable
+  def my_each(&block)
+    if block_given?
+      for item in self
+        block.call(item)
+      end
+      self
+    else
+      to_enum(__method__)
+    end
+  end
+end
+

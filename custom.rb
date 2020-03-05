d092ab6038d48451363cf89335215ea1c3f09f27
Nathan C <sihoonathan@gmail.com>
2020-03-04 20:38:44 -0800

    updated my_inject

--- custom.rb
+++ custom.rb
@@ -71,6 +71,82 @@ module Enumerable
     end
   end
 
+  def my_none?(pattern=nil, &block)
+    if block_given?
+      for elem in self
+        if block.call(elem)
+          return false
+        end
+      end
+      true
+    else
+      if pattern
+        for elem in self
+          if elem.class == pattern
+            return false
+          end
+        end
+        true
+      else
+        for elem in self
+          if !!elem == true
+            return false
+          end
+        end
+        true
+      end
+    end
+  end
+
+  def my_count(obj=nil, &block)
+    counter = 0
+    if block_given?
+      for elem in self
+        counter += 1 if block.call(elem)
+      end
+      counter
+    elsif obj
+      for elem in self
+        counter += 1 if elem == obj
+      end
+      counter
+    else
+      self.size
+    end
+  end
+
+  def my_map(&block)
+    ary = []
+    if block_given?
+      self.my_each {|each| ary << block.call(each)}
+      ary
+    else
+      self.to_enum(__method__)
+    end
+  end
+
+  def my_inject(initial=nil, &block)
+    if block_given?
+      if initial == nil
+        final = 0
+        self.my_each {|each| final = block.call(final, each)}
+        final
+      else
+        final = initial
+        self.my_each {|each| final = block.call(final, each)}
+        final
+      end
+    else
+      raise LocalJumpError.new("no block given")
+    end
+  end
+
 
 end
 
+
+
+p (5..10).my_inject {|sum, n| sum + n}
+p (5..10).my_inject(1) { |product, n| product * n }
+p (5..10).my_inject
+# p (5..10).inject
